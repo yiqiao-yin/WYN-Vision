ad9600de0ec99c139c0969bed191d6c1182a70be
Yiqiao Yin <eagle0504@gmail.com>
2023-06-29 19:14:33 -0400

    tensorflow installed

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 import tensorflow as tf
@@ -5,4 +7,13 @@ import tensorflow as tf
 # Load model
 new_model = tf.keras.models.load_model('toy_mnist_model.h5')
 
+# Load image
+img = cv2.imread('/content/sample.png')
+
+# Plot
+fig, ax = plt.subplots()
+ax.imshow(img, bins=20)
+st.pyplot(img)
+
+# Write
 st.write("hello world")
