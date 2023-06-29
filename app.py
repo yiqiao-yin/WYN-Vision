05be87ce8012c964992feedde86c33adb3eefe01
Yiqiao Yin <eagle0504@gmail.com>
2023-06-29 19:42:18 -0400

    tensorflow installed

--- app.py
+++ app.py
@@ -10,5 +10,11 @@ new_model = tf.keras.models.load_model('toy_mnist_model.h5')
 uploaded_file = st.file_uploader("Upload your file here...", type=['png', 'jpeg', 'jpg'])
 if uploaded_file is not None:
     st.image(uploaded_file)
+    st.write(type(uploaded_file))
+
+    # Inference
+    # pred = new_model.predict(uploaded_file)
+    # label = np.argmax(pred, axis=1)
+
 else:
-    st.warning("Please upload a csv file.")
+    st.warning("Please upload a jpg/png file.")
