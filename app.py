9af3346edbb7e7af5396e01f7b74d3bf710d6ace
Yiqiao Yin <eagle0504@gmail.com>
2023-07-05 14:45:26 -0400

    new function added

--- app.py
+++ app.py
@@ -68,7 +68,13 @@ st.markdown(
 # Sidebar
 task = st.sidebar.radio(
     "Choose a task:",
-    ("Image Classification", "Cancer Segmentation", "Denoise Image", "Text-to-Image", "Next"),
+    (
+        "Image Classification",
+        "Cancer Segmentation",
+        "Denoise Image",
+        "Text-to-Image",
+        "Next",
+    ),
 )
 st.sidebar.markdown(
     "@ [Yiqiao Yin](https://www.y-yin.io/) | [LinkedIn](https://www.linkedin.com/in/yiqiaoyin/) | [YouTube](https://youtube.com/YiqiaoYin/)"
@@ -77,10 +83,12 @@ st.sidebar.markdown(
 
 # Choose task
 if task == "Image Classification":
-    st.markdown("""
+    st.markdown(
+        """
         To learn more about image classification, please refer to this [notebook](https://github.com/yiqiao-yin/WYNAssociates/blob/main/docs/ref-deeplearning/ex02%20-%20ann%20and%20cnn.ipynb).
         ⚠️⚠️⚠️To interact with the app, you'll need a picture. You can find a sample picture [here](https://github.com/yiqiao-yin/WYN-Vision/tree/main/pics).
-    """)
+    """
+    )
     # Load model
     new_model = tf.keras.models.load_model("models/toy_mnist_model.h5")
     if new_model is not None:
@@ -108,7 +116,8 @@ if task == "Image Classification":
     else:
         st.warning("Please upload a jpg/png file.")
 elif task == "Cancer Segmentation":
-    st.markdown("""
+    st.markdown(
+        """
         H&E stained images from five prostate cancer Tissue Microarrays 
         (TMAs) and corresponding Gleason annotation masks. In the masks,
         pixel indices correspond to classes as follows: 0=Benign (green),
@@ -133,8 +142,11 @@ elif task == "Cancer Segmentation":
         Github source, see [here](https://github.com/eiriniar/gleason_CNN). 
 
         ⚠️⚠️⚠️To interact with the app, you'll need a picture. You can find a sample picture [here](https://github.com/yiqiao-yin/WYN-Vision/tree/main/pics).
-    """)
-    st.success("To demonstrate image segmentation task here, we use a binary mask generator by U-net trained on the above prostate cancer data. The mask highlights the cancerous regions, e.g. Gleason Score greater than 4.")
+    """
+    )
+    st.success(
+        "To demonstrate image segmentation task here, we use a binary mask generator by U-net trained on the above prostate cancer data. The mask highlights the cancerous regions, e.g. Gleason Score greater than 4."
+    )
     # Load model
     new_model = tf.keras.models.load_model("models/unet_6_6_allgleason_path1_.h5")
     if new_model is not None:
@@ -156,7 +168,6 @@ elif task == "Cancer Segmentation":
         st.write(f"Dimension of resized image: {image.shape}")
 
         # Inference
-        st.warning("AI is generating mask...")
         pred = new_model.predict(image.reshape((1, w, h, 3)))
         st.success("AI finished generating the mask!")
         mask = pred[0, :, :, 0]
@@ -177,13 +188,19 @@ elif task == "Cancer Segmentation":
                 st.pyplot(fig)
     else:
         st.warning("Please upload a jpg/png file.")
-
-
-
-
-
-
 elif task == "Denoise Image":
+    st.markdown(
+        """
+        **Autoencoders** are based on _Neural Networks (NNs)_ and are known as **Convolutional Neural Networks** (**CNNs** or **convnets**). A **convnet** is a __Deep Learning algorithm__ which takes an input image, assign importance (learnable weight, biases and retains spatial relationships in the data into each one of theirs layers) to various aspects/parts in the image and is able to differentiate/reconstruct the same.
+
+    The general idea behind this kind of code can be visualized here:
+
+    ![General autoencoder](https://www.pyimagesearch.com/wp-content/uploads/2020/02/keras_denoising_autoencoder_overview.png)
+
+    Then, the **autoencoder** compreehends an _encoder_ and a _decoder_. The **encoder** does the _encoding process_, i.e., transforms the image into a _compressed representation_ at the same time that starts the noisy reduction. Then, the _compressed representation_ goes to **decoder** that performs the _decoder process_, restoring the image to its true and recognizable shape. At the end of the process, we remove almost all noise in the image.
+    """
+    )
+
     # Load model
     new_model = tf.keras.models.load_model("models/denoising_mnist_ae_model.h5")
 
@@ -204,7 +221,6 @@ elif task == "Denoise Image":
         st.write(f"Dimension of resized image: {image.shape}")
 
         # Inference
-        st.warning("AI is denoisying the image...")
         pred = new_model.predict(image.reshape((1, w, h, 1)))
         st.success("AI finished generating denoising process!")
         denoised_img = pred[0, :, :, 0]
@@ -221,16 +237,12 @@ elif task == "Denoise Image":
                 fig, ax = plt.subplots()
                 ax.axis("off")
                 ax.imshow(image, cmap="Reds")
-                ax.imshow(denoised_img, alpha=np.round(float(alpha) / 100, 1), cmap="Reds")
+                ax.imshow(
+                    denoised_img, alpha=np.round(float(alpha) / 100, 1), cmap="Reds"
+                )
                 st.pyplot(fig)
     else:
         st.warning("Please upload a jpg/png file.")
-
-
-
-
-
-
 elif task == "Text-to-Image":
     with st.form(key="my_form"):
         text_prompt = st.text_input(
